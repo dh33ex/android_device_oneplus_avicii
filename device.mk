bd893f2926528cedb70c1abbd8309b67c980529b
dh33ex <dh33ex@riseup.net>
2025-02-21 16:30:25 +0000

    fix: built vendor.qti.hardware.capabilityconfigstore@1.0.so

--- device.mk
+++ device.mk
@@ -134,6 +134,10 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# ConfigStore
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
