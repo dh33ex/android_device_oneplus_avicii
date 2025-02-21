498d77ce3348b56ae98f8cc81995784c9e63c8f0
dh33ex <dh33ex@riseup.net>
2025-02-21 16:37:04 +0000

    develop: move WfdCommon boot jar declaration to vendor
    Based on https://github.com/LineageOS/android_device_oneplus_sm8250-common/commit/c3132f98bed4ab0e88931fa503858fb90e7c4057

--- device.mk
+++ device.mk
@@ -486,9 +486,6 @@ PRODUCT_PACKAGES += \
     libpng.vendor:32 \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
