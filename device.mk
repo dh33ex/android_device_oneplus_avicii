c3be28a014abb5ab51ee52e4c7cfb7ce911b9866
dh33ex <dh33ex@riseup.net>
2025-02-28 11:23:30 +0000

    develop: backport audio from sm8250
    Based on https://github.com/XxRagulxX/android_device_oneplus_avicii/commit/bc555e0e5ea038bcee28946324cc0cfcdb912f02

--- device.mk
+++ device.mk
@@ -269,9 +269,10 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
+    $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
+    $(LOCAL_PATH)/media/media_codecs_performance_lito_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_lito.xml \
+    $(LOCAL_PATH)/media/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/etc/video_system_specs.json
     
 PRODUCT_PACKAGES += \
     libavservices_minijail \
