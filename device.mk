70bf09506c6f56d2f08f59823b9f441b4251b8e8
dh33ex <dh33ex@riseup.net>
2025-02-28 11:42:58 +0000

    develop: disable ISunlightEnhancement via soong option
    Based on https://github.com/LineageOS/android_device_oneplus_kebab/commit/d4bfa56b3396823f533d492914948862dd0689bf

--- device.mk
+++ device.mk
@@ -303,6 +303,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
 
+# LiveDisplay
+$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
+
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
