085989a51d7b6fb0b32c4fdd2279d32080f64d2b
dh33ex <dh33ex@riseup.net>
2025-02-28 11:41:07 +0000

    develop: adjust override prop key names again
    Based on https://github.com/LineageOS/android_device_oneplus_kebab/commit/a55f64dbbf80c6ed7f0f3fd6aa7091a5cdfe1139

--- lineage_avicii.mk
+++ lineage_avicii.mk
@@ -26,6 +26,9 @@ PRODUCT_SYSTEM_DEVICE := Nord
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="Nord-user 12 RKQ1.211119.001 Q.202212051830:user release-keys"
-
-BUILD_FINGERPRINT := OnePlus/Nord/Nord:12/RKQ1.211119.001/Q.202212051830:user/release-keys
+    BuildDesc="Nord-user 12 RKQ1.211119.001 Q.202212051830:user release-keys" \
+    BuildFingerprint=OnePlus/Nord/Nord:12/RKQ1.211119.001/Q.202212051830:user/release-keys \
+    DeviceName=Nord \
+    DeviceProduct=Nord \
+    SystemDevice=Nord \
+    SystemName=Nord
