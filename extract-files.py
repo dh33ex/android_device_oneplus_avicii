ffdb7a2f8712f591cff3731d0838644a505b3948
dh33ex <dh33ex@riseup.net>
2025-04-08 19:16:06 +0000

    avicii: Patch libwfdservice to resolve duplicate dependencies
    Based on https://review.lineageos.org/c/LineageOS/android_device_oneplus_sm8250-common/+/405422

--- extract-files.py
+++ extract-files.py
@@ -115,6 +115,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('/my_product', '/product'),
     'system_ext/bin/wfdservice': blob_fixup()
         .add_needed('libwfdservice_shim.so'),
+    'system_ext/lib/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'system_ext/framework/oplus-ims-ext.jar': blob_fixup()
         .apktool_patch('blob-patches/oplus-ims-ext.patch', '-r'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
